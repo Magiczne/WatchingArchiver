71c72c36b3b92cb602708f6b30282517648a4026
Michał Kleszczyński <michalkamilkleszczynski@gmail.com>
2018-07-06 19:43:09 +0200

    UI + MainViewModel

---
+++ WatchingArchiver/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Caliburn.Micro;
+
+namespace WatchingArchiver.ViewModels
+{
+    internal class MainWindowViewModel : Screen
+    {
+        #region Properties
+
+        public ObservableCollection<string> Archived { get; set; }
+
+        public bool Archiving { get; set; }
+
+        public string ActionButtonText => Archiving ? "Stop archiving" : "Start archiving";
+
+        #endregion
+
+        #region Methods
+
+        public void ToggleAction()
+        {
+            Archiving = !Archiving;
+        }
+
+        #endregion
+    }
+}
---
+++ WatchingArchiver/Views/MainWindowView.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace WatchingArchiver.Views
+{
+    /// <inheritdoc cref="MahApps.Metro.Controls.MetroWindow" />
+    /// <summary>
+    ///     Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
